230d0aa5852bd4f23b78363c3c16f2a701b96417
jackavryashnik <jackavryashnik@gmail.com>
2024-07-07 17:39:03 +0200

    not found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import css from './NotFoundPage.module.css';
+import logo from '../../images/camper.png';
+import Button from '../../components/Button/Button';
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  const [counter, setCounter] = useState(10);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setCounter(counter - 1), 1000);
+
+    return () => clearTimeout(timer);
+  }, [counter]);
+
+  return (
+    <div className={css.page}>
+      <h2 className={css.subtitle}>Oops! The page you requested could not be found.</h2>
+      <div className={css.titleContainer}>
+        <h1 className={css.title}>
+          4<img src={logo} alt="logo" width={200} height={200} />4
+        </h1>
+      </div>
+      <div className={css.home}>
+        <p className={css.text}>But don&apos;t worry we&apos;ll get you back now.</p>
+        <p className={css.counter}>{counter}</p>
+      </div>
+      {!counter && <Navigate to="/" />}
+
+      <Button className={css.button} type={'button'} onClick={() => setCounter(0)}>
+        Go home now
+      </Button>
+    </div>
+  );
 };
 
 export default NotFoundPage;
