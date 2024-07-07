0b27d7494ecbd20d54589c120e84e0280ef98dbf
jackavryashnik <jackavryashnik@gmail.com>
2024-07-07 14:00:03 +0200

    create redux file structure

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://6686ad9a83c983911b033822.mockapi.io';
+
+export const getCampers = async () => {
+  try {
+    const { data } = await axios.get('/campers');
+    if (!data || data.length === 0) {
+      throw new Error('Campers not found');
+    }
+    console.log(data);
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
